434b54df8b9d9a7f0bf42a3134fac006c0e9a419
Rotem Harel <rotev@github.com>
2023-02-19 17:05:07 +0000

    Page with header

---
+++ app/pages/circle-of-fifths.js
@@ -0,0 +1,16 @@
+import styles from '../styles/CircleOfFifths.module.scss';
+
+export default function CircleOfFifths() {
+  return (
+    <div className={styles.circleOfFifths}>
+      <header>
+        <h1>Circle of Fifths</h1>
+      </header>
+      <section className={styles.main}>
+        <div className={styles.circle}>
+          
+        </div>
+      </section>
+    </div>
+  )
+}
